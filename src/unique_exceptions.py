c7cb674281d3ed4c1baeac371bd065d84e69b7d0
Sofia Kabir <sofia23kabir@gmail.com>
2024-05-04 13:41:51 +1000

    test exceptions

--- src/unique_exceptions.py
+++ src/unique_exceptions.py
@@ -2,23 +2,19 @@
 class InvalidTimeError(Exception):
     """An error raised when the end time is earlier than or equal to the start time."""
     def __init__(self):
-        super().__init__(
-            "End time must be later than start time."
-        )
+        super().__init__("End time must be later than start time.")
+                 
+class SameDayOverlapError(Exception):
+    """An error raised when a new class overlaps with another class on the same day."""
+    def __init__(self, message="Class overlaps with another class on the same day."):
+        self.message = message
+        super().__init__(self.message)
+
 class EmptyInputError(Exception):
     """An error raised when the user provides empty input for required fields."""
     def __init__(self):
         super().__init__("Please enter a value for this field.")
 
-class InvalidRoomError(Exception):
-    """An error raised when the room number input is not within a valid range or format."""
-    def __init__(self):
-        super().__init__("Invalid room number. Please enter a valid room number.")
-
-class DuplicateClassError(Exception):
-    """An error raised when the user tries to add a class with the same name and time as an existing class."""
-    def __init__(self):
-        super().__init__("A class with the same name and time already exists.")
 
 class ScheduleNotFoundError(Exception):
     """An error raised when the application cannot find the schedule file or encounters issues while reading or writing to it."""
