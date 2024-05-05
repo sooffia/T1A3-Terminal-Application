0b44d7351bb09294bc767d0731937b1a09fd712b
Sofia Kabir <sofia23kabir@gmail.com>
2024-05-05 23:38:07 +1000

    final edit

--- src/unique_exceptions.py
+++ src/unique_exceptions.py
@@ -20,13 +20,3 @@ class InvalidScheduleFormatError(Exception):
         self.message = message
         super().__init__(self.message) 
                  
-# class SameDayOverlapError(Exception):
-#     """An error raised when a new class overlaps with another class on the same day."""
-#     def __init__(self, message="Class overlaps with another class on the same day."):
-#         self.message = message
-#         super().__init__(self.message)
-
-# class InvalidDayError(Exception):
-#     """An error raised when an invalid day name is entered."""
-#     def __init__(self):
-#         super().__init__("Invalid day name. Please enter a valid day.")
