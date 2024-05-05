e4779222d84934cd8a916d1ae4289c2ed58d7312
Sofia Kabir <sofia23kabir@gmail.com>
2024-05-05 18:09:47 +1000

    added fourth error exception

--- src/unique_exceptions.py
+++ src/unique_exceptions.py
@@ -1,27 +1,32 @@
 
 class InvalidTimeError(Exception):
-    """An error raised when the end time is earlier than or equal to the start time."""
-    def __init__(self):
-        super().__init__("End time must be later than start time.")
-                 
-class SameDayOverlapError(Exception):
-    """An error raised when a new class overlaps with another class on the same day."""
-    def __init__(self, message="Class overlaps with another class on the same day."):
-        self.message = message
-        super().__init__(self.message)
+    """An error raised when an invalid time is entered."""
+    def __init__(self, message):
+        super().__init__(message) 
 
 class EmptyInputError(Exception):
     """An error raised when the user provides empty input for required fields."""
     def __init__(self):
         super().__init__("Please enter a value for this field.")
 
-
-class ScheduleNotFoundError(Exception):
-    """An error raised when the application cannot find the schedule file or encounters issues while reading or writing to it."""
+class InvalidClassSelectionError(Exception):
+    """An error raised when an invalid class number is selected."""
     def __init__(self):
-        super().__init__("Schedule file not found or inaccessible.")
+        super().__init__("Invalid class selection. Please enter a valid class number.")
 
-class InvalidDayError(Exception):
-    """An error raised when an invalid day name is entered."""
-    def __init__(self):
-        super().__init__("Invalid day name. Please enter a valid day.")
+class InvalidScheduleFormatError(Exception):
+    """Custom exception raised when the format of the schedule data is invalid."""
+    def __init__(self, message="Invalid schedule format."):
+        self.message = message
+        super().__init__(self.message) 
+                 
+# class SameDayOverlapError(Exception):
+#     """An error raised when a new class overlaps with another class on the same day."""
+#     def __init__(self, message="Class overlaps with another class on the same day."):
+#         self.message = message
+#         super().__init__(self.message)
+
+# class InvalidDayError(Exception):
+#     """An error raised when an invalid day name is entered."""
+#     def __init__(self):
+#         super().__init__("Invalid day name. Please enter a valid day.")
